cc52ad8f36e8712d32d30f8f32e6bb150d3cf997
nilocx <147480021+nilocx@users.noreply.github.com>
2023-10-12 17:17:30 +0200

    fenetre.py

--- fenetre.py
+++ fenetre.py
@@ -3,7 +3,7 @@ from tkinter import*
 
 fenetre=Tk()           #crée la fenêtre appelée fenetre
 fenetre.configure(bg="white")
-fenetre.title("LE PENDU") #Affiche le titre en haut de la fenêtre
+fenetre.title("SALON") #Affiche le titre en haut de la fenêtre
 fenetre.geometry("1300x800")  #redimensionner la fenêtre, l'unité est le pixel
 
 fenetre.mainloop()
