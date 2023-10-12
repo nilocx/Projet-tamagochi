e7f20b43cc4022ef0a0b84517069bcf4365a79f3
nilocx <147480021+nilocx@users.noreply.github.com>
2023-10-12 17:16:11 +0200

    Statistique du perso

---
+++ STAT_du_Perso_Tamagochie_cslg.py
@@ -0,0 +1,36 @@
+### Classe permetant la gestion des statistique du personnage.
+
+class Statistique:
+    def __init__(self):
+        #initialisation des variable
+        self.stat=[1,2,3,4,5,6,7,8,9,10]
+    def est_vide(self):
+        #verifie si la liste choisie est remplie ou non
+        if len(self.stat)==0:
+            return True and print('Vide/mort')
+        else:
+            return False and print('Toujours vivant, toujhour la bannane')
+    def ajoute(self,augmentation):
+        #ajoute une element a la liste choisi
+        if len(self.stat)==10:
+            return print("est plein")
+        else:
+            for i in range(augmentation):
+                self.stat.append(1)
+        return self.stat
+    def suprime(self,supression):
+        #suprime le dernier eleemnt de la liste choisie
+        for j in range(supression):
+            self.stat.pop()
+            if len(self.stat)==0:
+                print('Vide/mort')
+        return self.stat
+    def total(self):
+        #renvoi le contenue de la variable choisi
+        return self.stat
+
+# Definit les principale statistique du personnage
+faim=Statistique()
+energie=Statistique()
+depression=Statistique()
+
