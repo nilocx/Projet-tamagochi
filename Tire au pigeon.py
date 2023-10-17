81b5725968a69e06de80c3c53e5d732d9cd75ab1
nilocx <147480021+nilocx@users.noreply.github.com>
2023-10-17 08:54:20 +0200

    Update Tire au pigeon.py

--- Tire au pigeon.py
+++ Tire au pigeon.py
@@ -3,25 +3,23 @@ from STAT_du_Perso_Tamagochie_cslg import*
 from tkinter import*
 from PIL import Image, ImageTk
 
-def createPigeon(delay):
-    pigeon = ImageTk.PhotoImage(Image.open(r"pigeon.gif"))
-    y_position = randint(200,475)
-    my_photo = canvas.create_image(y_position, 80, image = piegon, anchor = 's')
-    root.after(delay,createPigeon)
+def createPigeon():
+    x_position = randint(100,200)
+    y_position = randint(100,200)
+    pigeon = ImageTk.PhotoImage(Image.open("pigeon.gif"))
+    my_photo = cvn.create_image(x_position, y_position, image = pigeon, anchor = 's')
+    fenetreTireauPigeon.after(3000,createPigeon)
+    print("teste")
 
-def movePigeon():
-    for fruit in fruits_list:
-        canvas.move(fruit[0], 0, fruit[1])
-        if canvas.bbox(fruit[0])[3] >= canvas.winfo_height()+100:
-            canvas.delete(fruit[0])
-            fruits_list.remove(fruit)
-    root.after(10, movePigeon)
+# def movePigeon(x,y):
+#     cvn.move(x,y)
+#     fenetreTireauPigeon.after(10, movePigeon)
 
 
 #creation de la fenetre
 fenetreTireauPigeon=Tk()
 fenetreTireauPigeon.title("Tire Au Pigeon")
-fenetreTireauPigeon.geometry("770x475")
+fenetreTireauPigeon.geometry("768x492")
 
 fond = Image.open("fond.gif")
 photo = ImageTk.PhotoImage(fond)
@@ -35,12 +33,20 @@ photo = ImageTk.PhotoImage(fond)
 # img.image = render
 # img.place(x=-1, y=0)
 
-cvn=Canvas(fenetreTireauPigeon, width=770,height=475)
-cvn.create_image(0,225, image=photo,anchor="center")
+cvn=Canvas(fenetreTireauPigeon, width=768,height=492)
+cvn.create_image(0,0, image=photo, anchor="nw")
 cvn.pack()
 
 #Boutton de destruction TEMPORAIre
 destruction=Button(fenetreTireauPigeon,text="DESTRUCTION",command=fenetreTireauPigeon.destroy)
+destruction.pack()
 destruction_w=cvn.create_window(200, 200, window=destruction)
+teste=Button(fenetreTireauPigeon,text="test",command=createPigeon())
+teste.pack()
+teste_w=cvn.create_window(100,100,window=teste)
+
+fruits_list = []
+
+# movePigeon(5,2)
 
 fenetreTireauPigeon.mainloop()
