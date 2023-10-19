6b2400966b179af6000b43f4a4454601205901aa
sadhamhussein <147731163+sadhamhussein@users.noreply.github.com>
2023-10-19 17:37:56 +0200

    Update mouv pas aleatoire.py
    y'a aussi un peu de bouffe dedans

--- mouv pas aleatoire.py
+++ mouv pas aleatoire.py
@@ -1,34 +1,38 @@
 from tkinter import*
-from random import randint
+import random
 import tkinter as tk
 import time
 global image_perso
+global img1,img2
+
 
 
 class Mouvement_alea(tk.Canvas):
     def __init__(self,*args, **kwargs):
         self.img=image_perso
+        self.tk=can
 
     def bas(self):
-        for i in range (3):
-            can.move(self.img,0,50)
+            self.move(self.img,0,25)
 
     def haut(self):
-        for i in range (3):
-            can.move(self.img,0,(-50))
+            self.move(self.img,0,(-25))
 
     def gauche(self):
-        for i in range (3):
-            can.move(self.img,-25,0)
+            self.move(self.img,-25,0)
 
     def droite(self):
-        for i in range (3):
-            can.move(self.img,25,0)
+            self.move(self.img,25,0)
 
 
 
 
-
+def alea(event):
+    global prev_x, prev_y
+    event.x=1
+    event.y=0
+    can.move(image_perso, event.x-prev_x, event.y-prev_y)
+    fenetre.after(5000,alea)
 
 
 def canvas_clique(event):
@@ -41,58 +45,42 @@ def canvas_glisse(event):
     can.move(image_perso, x - prev_x, y - prev_y)
     prev_x, prev_y = x, y
 
-def canvas_bouge_alea() :
-    tps_testeur=time.time()
-    bouge=Mouvement_alea(can)
-    if time.time()-tps_testeur >= 5.0:
-        bouge.bas()
-        can.after(800,canvas_bouge_alea())
+def bouffe():
+    img1=tk.PhotoImage(file="banane.PNG")
+    img2=tk.PhotoImage(file="banane.PNG")
+    fenetre.after(5000,bouffe)
 
 
 
 
 fenetre=tk.Tk()           #crée la fenêtre appelée fenetre
 fenetre.configure(bg="white")
-fenetre.title("TamaGotsho") #Affiche le titre en haut de la fenêtre
+fenetre.title("TamaGotshi") #Affiche le titre en haut de la fenêtre
 fenetre.geometry("1300x800")  #redimensionner la fenêtre, l'unité est le pixel
-can = Canvas(fenetre, width=1300, height=800, bg='black')
-
+can = Canvas(fenetre, width=2000, height=1200, bg='black')
+liste_bouffe=["banane.PNG","burger_apres_modif.PNG","wings_de_poulet.PNG","brocoli.PNG","past_que.PNG"]
 
 can.pack()
 
 
+
+
 image = tk.PhotoImage(file="test.png")
+img1=tk.PhotoImage(file="banane.PNG")
+img2=tk.PhotoImage(file="banane.PNG")
 # Afficher l'image sur le canvas
 image_perso = can.create_image(250, 250, anchor=tk.NW, image=image)
+image_bouffe1 = can.create_image(600, 250, anchor=tk.NW, image=img1)
+image_bouffe2 = can.create_image(500, 250, anchor=tk.NW, image=img2)
 
 # Lier des événements pour le déplacement de l'image
 prev_x, prev_y = 250, 250
 can.bind("<Button-1>", canvas_clique)
+can.bind("<ButtonRelease-1>", alea)
 can.bind("<B1-Motion>", canvas_glisse)
 
+
 bouge=Mouvement_alea(can)
-
-yBouton=220
-btnAjoute = tk.Button(fenetre, text="bouge", font=("tahoma Bold", 13), command=canvas_bouge_alea())
-btnAjoute.place(x=100, y=yBouton)
-
-
-
+bouffe()
 
 fenetre.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
