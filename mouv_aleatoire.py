9decbead66ee818a2dfebcbf0674caf21e064cc7
sadhamhussein <147731163+sadhamhussein@users.noreply.github.com>
2023-11-06 14:43:56 +0100

    Update mouv_aleatoire.py
    maintenant y'a les com

--- mouv_aleatoire.py
+++ mouv_aleatoire.py
@@ -1,58 +1,56 @@
 import tkinter as tk
 import random
 
-# Fonction pour déplacer l'image de manière aléatoire
-def mouvement_alea():
-    if not img_prise:  # Vérifier si l'image est tenue par le joueur
-        x, y = random.randint(0, canvas_width - image_width), random.randint(0, canvas_height - image_height)
-        can.coords(image_perso, x, y)  # Déplacer l'image à de nouvelles coordonnées aléatoires
-    fenetre.after(60000, mouvement_alea)  # Appeler mouvement_alea toutes les minutes
+class TamaGotshi:
+    def __init__(self):
+        self.fenetre = tk.Tk()
+        self.fenetre.title("TamaGotshi")
+        self.fenetre.geometry("1900x1000")
 
-# Fonction appelée lorsqu'un clic de souris est détecté sur le canvas
-def canvas_clique(event):
-    global prev_x, prev_y, img_prise
-    prev_x, prev_y = event.x, event.y
-    img_prise = True
+        self.canvas_width = 1900
+        self.canvas_height = 1000
+        
+        # Création d'un canvas pour afficher l'image
+        self.can = tk.Canvas(self.fenetre, width=self.canvas_width, height=self.canvas_height, bg='black')
+        self.can.pack()
+        
+        # Chargement de l'image à afficher sur le canvas
+        self.image = tk.PhotoImage(file="test.png")
+        self.image_width = self.image.width()
+        self.image_height = self.image.height()
+        self.image_perso = self.can.create_image(250, 250, anchor=tk.NW, image=self.image)
 
-# Fonction appelée lorsque le bouton de la souris est relâché
-def canvas_relache(event):
-    global img_prise
-    img_prise = False
+        self.prev_x, self.prev_y = 250, 250
+        self.img_prise = False # Variable pour indiquer si l'image est tenue
+        
+        # appelle la fonction correspondante au action fait par le joueur avec la souris
+        self.can.bind("<Button-1>", self.canvas_clique)
+        self.can.bind("<ButtonRelease-1>", self.canvas_relache)
+        self.can.bind("<B1-Motion>", self.canvas_glisse)
 
-# Fonction appelée lorsqu'un glissement de souris est détecté sur le canvas
-def canvas_glisse(event):
-    global prev_x, prev_y
-    x, y = event.x, event.y
-    can.move(image_perso, x - prev_x, y - prev_y)  # Déplacer l'image en fonction du glissement de souris
-    prev_x, prev_y = x, y
+        self.mouvement_alea() # Appelle la fonction de mouvement aléatoire dès le lancement du jeu
 
-# Initialisation de la fenêtre Tkinter
-fenetre = tk.Tk()
-fenetre.title("TamaGotshi")
-fenetre.geometry("1900x1000")
+    def mouvement_alea(self): # Fonction pour déplacer l'image de manière aléatoire
+        if not self.img_prise: # Vérifier si l'image est tenue par le joueur
+            x, y = random.randint(0, self.canvas_width - self.image_width), random.randint(0, self.canvas_height - self.image_height)
+            self.can.coords(self.image_perso, x, y)  # Déplacer l'image à de nouvelles coordonnées aléatoires
+        self.fenetre.after(60000, self.mouvement_alea)
 
-canvas_width = 1900
-canvas_height = 1000
+    def canvas_clique(self, event):  # Fonction appelée lorsqu'un clic de souris est détecté sur le canvas
+        self.prev_x, self.prev_y = event.x, event.y
+        self.img_prise = True
 
-# Création d'un canvas pour afficher l'image
-can = tk.Canvas(fenetre, width=canvas_width, height=canvas_height, bg='black')
-can.pack()
+    def canvas_relache(self, event):  # Fonction appelée lorsque le bouton de la souris est relâché
+        self.img_prise = False
 
-# Chargement de l'image à afficher sur le canvas
-image = tk.PhotoImage(file="test.png")
-image_width = image.width()
-image_height = image.height()
-image_perso = can.create_image(250, 250, anchor=tk.NW, image=image)
+    def canvas_glisse(self, event): # Fonction appelée lorsqu'un glissement de souris est détecté sur le canvas
+        x, y = event.x, event.y
+        self.can.move(self.image_perso, x - self.prev_x, y - self.prev_y)
+        self.prev_x, self.prev_y = x, y
 
-prev_x, prev_y = 250, 250
-img_prise = False  # Variable pour indiquer si l'image est tenue
+    def run(self):
+        self.fenetre.mainloop()
 
-# appelle la fonction correspondante au action fait par le joueur avec la souris
-can.bind("<Button-1>", canvas_clique)
-can.bind("<ButtonRelease-1>", canvas_relache)
-can.bind("<B1-Motion>", canvas_glisse)
-
-mouvement_alea()  # Appelle la fonction de mouvement aléatoire dès le lancement du jeu
-
-fenetre.mainloop()
-
+if __name__ == "__main__":
+    app = TamaGotshi()
+    app.run()
